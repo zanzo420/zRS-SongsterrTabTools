cc6083354171fa4cb8b641bac537ef9695e3a9fe
Zanzo <zp2kshield@gmail.com>
2023-11-04 19:43:50 -0700

    first commit

---
+++ Untitled2.ps1
@@ -0,0 +1,14 @@
+﻿$files = gci -Path "D:\GitHub\ultimate-guitar-downloader\output\" -Name -exclude *.gp*
+foreach($f in $files)
+{
+    $fFullName = $f.ToString()
+    $fName = ''
+    $fExt = ''
+    if($f[$f.length-4] -ne '.')
+    {
+        write-host $f -ForegroundColor Green
+    }else{
+        write-output $f
+    }
+    $f.substring($f.length - 4)
+}
---
+++ Untitled3.ps1
@@ -0,0 +1,5 @@
+﻿foreach($l in $testing.data){write-host $l.file_pc_link; $l.file_pc_link | out-file H:\IgnitionCDLCTools\DownloadURLs.txt -Append -Force}
+
+
+$gurls = gc -Path H:\IgnitionCDLCTools\DownloadURLs.txt
+foreach($u in $gurls){Start-Process chrome.exe $u}
